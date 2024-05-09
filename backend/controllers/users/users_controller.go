ea9de7ce43f83b1bef5c6e81c490022182ca0ca0
mateo <2210317@ucc.edu.ar>
2024-05-09 20:37:48 -0300

    "primero"

---
+++ backend/controllers/users/users_controller.go
@@ -0,0 +1,17 @@
+package users
+
+import (
+	usersDomain "backend/domain/users"
+	usersService "backend/services/users"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Login(context *gin.Context) {
+	var loginRequest usersDomain.LoginRequest
+	context.BindJSON(&loginRequest)
+	response := usersService.Login(loginRequest)
+	context.JSON(http.StatusOK, response)
+
+}
---
+++ backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"backend/router"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() { //Unica funcion es inicializar
+
+	engine := gin.New()
+	router.MapUrls(engine)
+	engine.Run(":8008")
+
+}
