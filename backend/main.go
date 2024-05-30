66555835b293cbdd087878e2ca0db592a8ef14d5
soficuozzo <2313251@ucc.edu.ar>
2024-05-29 21:48:33 -0300

    Cambios

--- backend/main.go
+++ backend/main.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() { //Unica funcion es inicializar
-    r := gin.Default()
-
+   
 	db.Init()
 
 	engine := gin.New()
 	router.MapUrls(engine)
 	engine.Run(":8080")
     // Configurar rutas 
+    r := gin.Default()
     router.SetupRoutes(r, db)
     if err := r.Run(":8080"); err != nil {
         log.Fatalf("failed to run server: %v", err)
