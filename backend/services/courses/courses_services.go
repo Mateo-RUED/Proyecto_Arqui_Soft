9a25044cc37763c77f5094895096d5ec350f6666
soficuozzo <2313251@ucc.edu.ar>
2024-05-30 10:15:18 -0300

    Probando sol errores

--- backend/services/courses/courses_services.go
+++ backend/services/courses/courses_services.go
@@ -2,13 +2,13 @@ package services
 
 import (
 
-    "backend/db"
+    // "backend/db"
     "backend/domain/courses" 
     "backend/dtos"
     "gorm.io/gorm"
   
 )
-
+/* 
 type CourseService struct {
     DB *gorm.DB
 }
@@ -17,36 +17,36 @@ func NewCourseService(db *gorm.DB) *CourseService {
     return &CourseService{
         DB: db,
     }
-}
+} */
 
 func (s *CourseService) CreateCourse(courseDTO dtos.CreateCourseDTO) (*domain.Course, error) {
     course := domain.Course{
         Name:        courseDTO.Name,
         Description: courseDTO.Description,
     }
-    if err := s.DB.Create(&course).Error; err != nil {
+  /*   if err := s.DB.Create(&course).Error; err != nil {
         return nil, err
-    }
+    } */
     return &course, nil
 }
 
 func (s *CourseService) UpdateCourse(id uint, courseDTO dtos.UpdateCourseDTO) (*domain.Course, error) {
     var course domain.Course
-    if err := s.DB.First(&course, id).Error; err != nil {
+    /* if err := s.DB.First(&course, id).Error; err != nil {
         return nil, err
-    }
+    } */
     course.Name = courseDTO.Name
     course.Description = courseDTO.Description
-    if err := s.DB.Save(&course).Error; err != nil {
+ /*    if err := s.DB.Save(&course).Error; err != nil {
         return nil, err
-    }
+    } */
     return &course, nil
 }
-
+/* 
 func (s *CourseService) DeleteCourse(id uint) error {
     var course domain.Course
     if err := s.DB.First(&course, id).Error; err != nil {
         return err
     }
     return s.DB.Delete(&course).Error
-}
+} */
