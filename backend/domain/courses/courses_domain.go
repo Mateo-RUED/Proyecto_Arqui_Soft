a21909da36af487d88f1c7f26dbf806702fe933c
mateo <2210317@ucc.edu.ar>
2024-06-07 17:19:52 -0300

    cambios

--- backend/domain/courses/courses_domain.go
+++ backend/domain/courses/courses_domain.go
@@ -4,6 +4,8 @@ type Course struct {
     ID          uint    `gorm:"primaryKey"`
     Name        string  `gorm:"not null"`
     Description string  `gorm:"not null"`
-    Precio       float64 `gorm:"not null"`
     Category     string  `gorm:"not null"`
+    Requisitos string `gorm:"not null"`
+    Duracion string `gorm:"not null"`
+    Imagen_url string `gorm:"not null"`
 }
