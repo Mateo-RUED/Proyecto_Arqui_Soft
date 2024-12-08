64233288584169b9864b0c8951d2970ff64b465e
mateo <2210317@ucc.edu.ar>
2024-12-08 16:56:55 -0300

    actualiza curso y home

--- frontend-react/src/Paginas/Cursos.js
+++ frontend-react/src/Paginas/Cursos.js
@@ -1,206 +1,84 @@
-import React from 'react'
-import styles from './Cursos.module.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './Cursos.module.css';
+
+const CursoCard = ({ curso }) => (
+  <div className={styles.card}>
+    <div className="card mb-4 bg-secondary-subtle">
+      <div className="row g-0">
+        <div className="col-md-2">
+          <img src={curso.imagen_url} className="card-img-top" alt={`Imagen del curso ${curso.name}`} />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{curso.name}</h5>
+            <p className="card-text">{curso.description}</p>
+            <p className="card-text">
+            <small className="text-body-secondary">
+              • <b>Requisitos:</b> {curso.requisitos} <br />
+              • <b>Duración:</b> {curso.duracion} <br />
+              • <b>Categoría:</b> {curso.category}
+            </small>
+          </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 const Cursos = () => {
+  const [cursos, setCursos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCursos = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/courses/all');
+        setCursos(response.data.courses);
+      } catch (err) {
+        setError('Error al cargar los cursos.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCursos();
+  }, []);
+
+  if (loading) return <div className={styles.loader}>Cargando...</div>;
+  if (error) return <div className={styles.error}>{error}</div>;
+
   return (
-    //La forma de poder darle stilos a las pag en react es a traves de className={styles.}, en este caso le estasmos dando el estilo al fondo de la misma
-    <div className={styles.fondo}> <br />
-
-      {/*La forma de encuadrar un contenido es a través del bloque de construcción container*/}
-      <div class="container">
-
-        <div class="card text-bg-dark">
-          
-          {/*En react las imagenes se agregan a través del codigo src={require("...")}*/}
-          <img src={require("../img/fondoCursos.jpg")} class="card-img" alt="..."/>
-
-          <div class="card-img-overlay">
-
-            <h1 class="card-title">Bienvenido a la Sección Cursos</h1>
-            <h3 class="card-text">En este apartado te mostraremos todos los cursos con los que contamos.</h3>
-
+    <div className={styles.fondo}>
+      <div className="container">
+        <div className="card text-bg-dark">
+          <img src={require('../img/fondoCursos.jpg')} className="card-img" alt="Fondo de Cursos" />
+          <div className="card-img-overlay">
+            <h1 className="card-title">Bienvenido a la Sección Cursos</h1>
+            <h3 className="card-text">En este apartado te mostraremos todos los cursos con los que contamos.</h3>
           </div>
-
         </div>
-
       </div>
 
-
-      <br/><br/><br/>
-
-
-      {/*En este caso el className={styles.tarjetas} se encargar de darle el tamaño necesario a nuestras tarjetas*/}
       <div className={styles.tarjetas}>
-        {/*En className={styles.card} lo que hacemos es darle ese efecto de zoom a la hora de pasar el cursor por encima de la tarjeta*/}
-        <div className={styles.card}>
-          <div class="card mb-4 bg-secondary-subtle">
-            <div class="row g-0">
-              {/*Con col-md-2 modificamos el tamaño de la tarjeta*/}
-              <div class="col-md-2">
-                <img src={require("../img/nuevojava.png")} class="card-img-top"/>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Java</h5>
-                  <p class="card-text">Nuestro programa está diseñado para equiparte con habilidades avanzadas en desarrollo backend utilizando JAVA,
-                    incluyendo frameworks como Spring Boot, manejo de bases de datos, y mejores prácticas en seguridad y diseño de software.</p>
-                  <p class="card-text"><small class="text-body-secondary">
-                    • <b>Requisitos:</b> <br/>
-                    &nbsp;&nbsp;- Entender programación básica, incluyendo variables, tipos de datos y estructuras de control. <br/>
-                    &nbsp;&nbsp;- Poseer una noción básica sobre desarrollo backend. <br/>
-                    &nbsp;&nbsp;- Familiaridad con IDs y sistemas de control de versiones como Git. <br/>
-                    • <b>Duración:</b> <br/>
-                    &nbsp;&nbsp;- 14 Semanas. <br />
-                    • <b>Profesor:</b> <br/>
-                    &nbsp;&nbsp;- Profesor E.<br />
-                    • <b>Categoria:</b> <br/>
-                    &nbsp;&nbsp;- Programación.
-                  </small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-
-        <div className={styles.card}>
-          <div class="card mb-4 bg-secondary-subtle">
-            <div class="row g-0">
-              <div class="col-md-2">
-                <img src={require("../img/nuevohtml.png")} class="card-img-top"/>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">HTML y CSS</h5>
-                  <p class="card-text">En este curso aprenderás a crear tu sitio web partiendo del prototipo en papel. Te sumergirás en las mejores
-                  prácticas del desarrollo web, trabajando con HTML y CSS.  </p>
-                  {/*EL text-body-secondary se utiliza para darle el color de fondo a las tarjetas*/}
-                  <p class="card-text"><small class="text-body-secondary">
-                    • <b>Requisitos:</b> <br/>
-                    &nbsp;&nbsp;- Para este curso no es necesario tener conocimientos previos. <br/>
-                    &nbsp;&nbsp;- Para mejorar tu experiencia de cursada, te aconsejamos tener una computadora con 2 GB de memoria RAM, <br/>
-                    &nbsp;&nbsp;&nbsp; procesador de dos núcleos y GPU de 2 GB de RAM. <br/>
-                    • <b>Duración:</b> <br/>
-                    &nbsp;&nbsp;- 10 Semanas<br />
-                    • <b>Profesor:</b> <br/>
-                    &nbsp;&nbsp;- Profesor A.<br />
-                    • <b>Categoria:</b> <br/>
-                    &nbsp;&nbsp;- Desarrollo Web.
-                  </small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div className={styles.card}>
-          <div class="card mb-4 bg-secondary-subtle">
-            <div class="row g-0">
-              <div class="col-md-2">
-                <img src={require("../img/JavaScript.png")} class="card-img-top"/>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">JavaScript</h5>
-                  <p class="card-text">En este curso aprenderás los fundamentos del lenguaje de programación más usado en la actualidad, con el cual
-                    es posible crer aplicaciones de todo tipo. Explorarás inicialmente herramientas propias del mismo, indagando casos prácticos de
-                    aplicación.</p>
-                  <p class="card-text"><small class="text-body-secondary">
-                    • <b>Requisitos:</b> <br/>
-                    &nbsp;&nbsp;- Tener conocimientos intermedios de HTML, CSS. <br/>
-                    &nbsp;&nbsp;- Poseer una noción básica sobre programación lógica. <br/>
-                    • <b>Duración:</b> <br/>
-                    &nbsp;&nbsp;- 8 Semanas.<br />
-                    • <b>Profesor:</b> <br/>
-                    &nbsp;&nbsp;- Profesor E.<br />
-                    • <b>Categoria:</b> <br/>
-                    &nbsp;&nbsp;- Programación.
-                  </small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-
-        <div className={styles.card}>
-          <div class="card mb-4 bg-secondary-subtle">
-            <div class="row g-0">
-              <div class="col-md-2">
-                <img src={require("../img/sql.png")} class="card-img-top"/>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">SQL</h5>
-                  <p class="card-text">En este curso aprenderás las nociones centrales de las bases de datos relacionales, las cuales son implementadas
-                    por todas las organizaciones para poder tomar decisiones con base en la información que generan en su modelo de negocio.</p>
-                  <p class="card-text"><small class="text-body-secondary">
-                    • <b>Requisitos:</b> <br/>
-                    &nbsp;&nbsp;- Tener conocimientos sobre otros lenguajes de programación. <br/>
-                    &nbsp;&nbsp;- PC con 4 GB de memoria RAM, procesador de cuatro núcleos y GPU de 2 GB de RAM. <br/>
-                    • <b>Duración:</b> <br/>
-                    &nbsp;&nbsp;- 10 Semanas<br />
-                    • <b>Profesor:</b> <br/>
-                    &nbsp;&nbsp;- Profesor B.<br />
-                    • <b>Categoria:</b> <br/>
-                    &nbsp;&nbsp;- Base de Datos.
-                  </small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div className={styles.card}>
-          <div class="card mb-4 bg-secondary-subtle">
-            <div class="row g-0">
-              <div class="col-md-2">
-                <img src={require("../img/nuevopython.png")} class="card-img-top"/>
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Python</h5>
-                  <p class="card-text">Te sumergirás en el mundo de la programación con Python, aprendiendo desde los conceptos básicos hasta la
-                    programación orientada a objetos. Desarrollarás habilidades en manejo de datos, definición de funciones y resolución de problemas
-                    complejos mediante el diseño de algoritmos.</p>
-                  <p class="card-text"><small class="text-body-secondary">
-                    • <b>Requisitos:</b> <br/>
-                    &nbsp;&nbsp;- Comprender conceptos matemáticos fundamentales, como operaciones aritméticas. <br/>
-                    &nbsp;&nbsp;- Habilidad para descomponer y analizar problemas. <br/>
-                    &nbsp;&nbsp;- Capacidad para analizar y resolver problemas de programación de nivel intermedio. <br/>
-                    • <b>Duración:</b> <br/>
-                    &nbsp;&nbsp;- 13 Semenas.<br />
-                    • <b>Profesor:</b> <br/>
-                    &nbsp;&nbsp;- Profesor F.<br />
-                    • <b>Categoria:</b> <br/>
-                    &nbsp;&nbsp;- Programación.
-                  </small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        {cursos.map(curso => (
+          <CursoCard key={curso.id} curso={curso} />
+        ))}
       </div>
 
       <div className={styles.piePagina}>
-
         <fieldset>
-        
           <legend>Sobre el Curso</legend>
-            
-          <p>Udemy es una <b>plataforma de educación online en vivo</b> creada con la misión de democratizar la educacion de calidad en toda Latinoamérica. Queremos
-          que nuestros alumnos estén preparados de la mejor manera para su salidad al mundo laboral.</p>
-            
+          <p>
+            Udemy es una <b>plataforma de educación online en vivo</b> creada con la misión de democratizar la educación
+            de calidad en toda Latinoamérica. Queremos que nuestros alumnos estén preparados de la mejor manera para su
+            salida al mundo laboral.
+          </p>
         </fieldset>
-
       </div>
-
     </div>
+  );
+};
 
-  )
-
-}
-
-export default Cursos
+export default Cursos;
--- frontend-react/src/Paginas/Home.js
+++ frontend-react/src/Paginas/Home.js
@@ -1,60 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import styles from './Home.module.css';
 
 const Home = ({ searchTerm }) => {
   const [filteredCursos, setFilteredCursos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const cursos = [
-    {
-      id: 1,
-      title: "HTML y CSS",
-      duration: "10 Semanas",
-      instructor: "Profesor A",
-      category: "Desarrollo Web",
-      image: require("../img/OIP.jpg")
-    },
-    {
-      id: 2,
-      title: "SQL",
-      duration: "10 Semanas",
-      instructor: "Profesor B",
-      category: "Bases de Datos",
-      image: require("../img/sql.png")
-    },
-    {
-      id: 3,
-      title: "JavaScript",
-      duration: "8 Semanas",
-      instructor: "Profesor C",
-      category: "Programación",
-      image: require("../img/JavaScript.png")
-    },
-    {
-      id: 5,
-      title: "Java",
-      duration: "14 Semanas",
-      instructor: "Profesor E",
-      category: "Programación",
-      image: require("../img/Java.jpg")
-    },
-    {
-      id: 6,
-      title: "Python",
-      duration: "13 Semanas",
-      instructor: "Profesor F",
-      category: "Programación",
-      image: require("../img/python.jpg")
-    }
-  ];
+  // Obtener los cursos desde la base de datos
+  useEffect(() => {
+    const fetchCursos = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/courses/all');
+        setFilteredCursos(response.data.courses);
+      } catch (err) {
+        setError('Error al cargar los cursos.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCursos();
+  }, []);
 
   useEffect(() => {
-    const term = searchTerm.toLowerCase();
-    const filtered = cursos.filter(curso => 
-      curso.title.toLowerCase().includes(term) || 
-      curso.category.toLowerCase().includes(term)
-    );
-    setFilteredCursos(filtered);
-  }, [searchTerm]);
+    if (searchTerm) {
+      const term = searchTerm.toLowerCase();
+      const filtered = filteredCursos.filter(curso =>
+        curso.name.toLowerCase().includes(term) ||
+        curso.category.toLowerCase().includes(term)
+      );
+      setFilteredCursos(filtered);
+    }
+  }, [searchTerm, filteredCursos]);
+
+  if (loading) return <div className={styles.loader}>Cargando...</div>;
+  if (error) return <div className={styles.error}>{error}</div>;
 
   return (
     <div className={styles.fondo}>
@@ -63,14 +43,14 @@ const Home = ({ searchTerm }) => {
         <h1>Unite a la comunidad de aprendizaje online en vivo más grande de Latinoamérica</h1>
         <p>Clases online en vivo dictadas por expertos de la industria, enfoque 100% práctico, mentorías personalizadas y acceso a una comunidad de +100.000 estudiantes.</p>
       </div>
-      <br></br>
-      
+      <br />
+
       {/* Profesores */}
       <div className={styles.profesores}>
-        <h1>Educación online que funciona </h1>
+        <h1>Educación online que funciona</h1>
         <div className={styles.profesores_descr}>
           <h2>Clases online en vivo con Profesores Expertos</h2>
-          <p>En nuestros cursos online en vivo vas a tener interacción real e instantánea con tu profesor, tutor y compañeros, <br/> para resolver todas tus dudas y avanzar en tus proyectos prácticos.</p>
+          <p>En nuestros cursos online en vivo vas a tener interacción real e instantánea con tu profesor, tutor y compañeros, para resolver todas tus dudas y avanzar en tus proyectos prácticos.</p>
           <ul>
             <li>Interacción en vivo.</li>
             <li>Corrección de proyectos prácticos.</li>
@@ -78,16 +58,16 @@ const Home = ({ searchTerm }) => {
           </ul>
         </div>
         <div>
-          <img src={require("../img/profesor.jpg")} alt="Profesor" /> 
+          <img src={require("../img/profesor.jpg")} alt="Profesor" />
         </div>
-        <br></br><br></br><br></br>
+        <br /><br /><br />
       </div>
 
       {/* Estudiantes */}
       <div className={styles.estudiantes}>
         <div className={styles.estudiantes_descr}>
           <h2>Aprendé junto a tus compañeros</h2>
-          <p>Está demostrado que aprender en grupo es más eficiente y motivador. El networking con tus compañeros de clase <br/> ayuda a que puedas tener nuevas ideas y hacer mejores proyectos.</p>
+          <p>Está demostrado que aprender en grupo es más eficiente y motivador. El networking con tus compañeros de clase ayuda a que puedas tener nuevas ideas y hacer mejores proyectos.</p>
           <ul>
             <li>Clases en grupo</li>
             <li>Canales de chat</li>
@@ -98,7 +78,7 @@ const Home = ({ searchTerm }) => {
           <img src={require("../img/estudiantes.jpg")} alt="Estudiantes" />
         </div>
       </div>
-      
+
       <div className={styles.cursos}>
         <section id={styles.cards}>
           <div className="container">
@@ -110,14 +90,13 @@ const Home = ({ searchTerm }) => {
               {filteredCursos.map((curso) => (
                 <div key={curso.id} className="col-md-3 g-5">
                   <div className="card text-center">
-                    <img src={curso.image} className="card-img-top" alt={curso.title} />
+                    <img src={curso.imagen_url} className="card-img-top" alt={curso.name} />
                     <div className={styles.cardbody}>
-                      <h5 className={styles.cardtitle}>{curso.title}</h5>
+                      <h5 className={styles.cardtitle}>{curso.name}</h5>
                       <p>
-                        Duración: {curso.duration} <br />
-                        Profesor: {curso.instructor} <br />
-                        Categoría: {curso.category}
-                      </p> 
+                        Duración: {curso.duracion} <br />
+                        Categoría: {curso.category} <br />
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -125,13 +104,13 @@ const Home = ({ searchTerm }) => {
             </div>
           </div>
         </section>
-      </div> 
+      </div>
 
       <div className={styles.piePagina}>
         <fieldset>
           <legend>Sobre el Curso</legend>
           <p>
-            Udemy es una <b>plataforma de educación online en vivo</b> creada con la misión de democratizar la educacion de calidad en toda Latinoamérica. Queremos
+            Udemy es una <b>plataforma de educación online en vivo</b> creada con la misión de democratizar la educación de calidad en toda Latinoamérica. Queremos
             que nuestros alumnos estén preparados de la mejor manera para su salida al mundo laboral.
           </p>
         </fieldset>
